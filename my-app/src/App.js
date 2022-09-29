18ad43a97125d6932e87abbf582e56f36c7ffdec
ShannonGW <ShannonWilliams93@gmail.com>
2022-09-29 19:19:46 +0200

    add middle name button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import MiddleName from "./components/MiddleName";
 
 function App() {
   return (
     <div className="App">
       <h1>Baby Name Generator</h1>
+      <h2>
+        <MiddleName />
+      </h2>
     </div>
   );
 }
---
+++ my-app/src/components/MiddleName.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MiddleName() {
+  return (
+    <>
+      <button>Click for Random Name</button>
+    </>
+  );
+}
+
+export default MiddleName;
