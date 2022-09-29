186331db266801c93af029d283d174b379c3ea6e
ShannonGW <ShannonWilliams93@gmail.com>
2022-09-29 19:22:11 +0200

    add first  name button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
 import MiddleName from "./components/MiddleName";
+import FirstName from "./components/FirstName";
 
 function App() {
   return (
     <div className="App">
       <h1>Baby Name Generator</h1>
       <h2>
+        Middle Name
         <MiddleName />
+      </h2>
+      <h2>
+        First Name
+        <FirstName />
       </h2>
     </div>
   );
---
+++ my-app/src/components/FirstName.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function FirstName() {
+  return (
+    <>
+      <button>Click for Random Name</button>
+    </>
+  );
+}
+
+export default FirstName;
