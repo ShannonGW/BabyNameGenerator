5108008e0e81750bb5ae1375d866ea26c263c58f
ShannonGW <ShannonWilliams93@gmail.com>
2022-09-29 20:45:30 +0200

    map through data array

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,14 @@
 import "./App.css";
 import MiddleName from "./components/MiddleName";
-import FirstName from "./components/FirstName";
-
+import UseFirstName from "./components/FirstName";
+import { data } from "./data";
 function App() {
   return (
     <div className="App">
-      <h1>Baby Name Generator</h1>
-
-      <h2>
-        First Name
-        <FirstName />
-      </h2>
-      <h2>
-        Middle Name
-        <MiddleName />
-      </h2>
+      <h1>Gender Neutral Baby Name Generator </h1> First Name
+      <UseFirstName />
+      Middle Name
+      <MiddleName />
     </div>
   );
 }
--- my-app/src/components/FirstName.js
+++ my-app/src/components/FirstName.js
@@ -1,19 +1,29 @@
 import React, { useState } from "react"; //named function we need to use {} and correct name
+import { data } from "../data";
 
 const UseFirstName = () => {
-  const [firstName, setFirstName] = useState("");
+  const [firstName, setFirstName] = useState(data);
 
-  const handleClick = () => {
-    if (firstName === "") {
-      setFirstName("Rose");
-    } else {
-      setFirstName("Clover");
-    }
-  };
+  //   const handleClick = () => {
+  //     // if (firstName === "") {
+  //     //   setFirstName("Rose");
+  //     // } else {
+  //     //   setFirstName("Clover");
+  //     // }
+  //     // let newFirstName = firstName.filter((firstName) => firstName.id !== id);
+  //     // setFirstName(firstName);
+  //   };
   return (
     <>
-      <h1>{firstName}</h1>
-      <button onClick={handleClick}>Click for Random First Name</button>
+      {data.map((name1) => {
+        const { id, name } = name1;
+        return (
+          <div key={id}>
+            <h4>{name}</h4>
+          </div>
+        );
+      })}
+      {/* <button onClick={handleClick}>Click for Random First Name</button> */}
     </>
   );
 };
